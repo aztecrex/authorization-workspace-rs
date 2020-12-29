c6263bbd1182f2e9dfe1dc8745049d1cc55841f5
Greg Wiley <aztec.rex@jammm.com>
2020-12-29 13:14:10 -0800

    reduce optional effects

--- src/effect.rs
+++ src/effect.rs
@@ -14,6 +14,14 @@ pub fn reduce_effects(e1: Effect, e2: Effect) -> Effect {
     }
 }
 
+pub fn reduce_optional_effects(e1: Option<Effect>, e2: Option<Effect>) -> Option<Effect> {
+    match (e1, e2) {
+        (None, x) => x,
+        (x, None) => x,
+        (Some(x), Some(y)) => Some(reduce_effects(x, y)),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -25,5 +33,27 @@ mod tests {
         assert_eq!(reduce_effects(ALLOW, ALLOW), ALLOW);
         assert_eq!(reduce_effects(DENY, DENY), DENY);
         assert_eq!(reduce_effects(DENY, ALLOW), DENY);
+    }
+
+    #[test]
+    fn test_reduce_optional() {
+        fn check_reduce_optional(e1: Effect, e2: Effect) {
+            assert_eq!(
+                reduce_optional_effects(Some(e1), Some(e2)),
+                Some(reduce_effects(e1, e2))
+            );
+        }
+
+        check_reduce_optional(ALLOW, DENY);
+        check_reduce_optional(ALLOW, ALLOW);
+        check_reduce_optional(DENY, ALLOW);
+        check_reduce_optional(DENY, DENY);
+
+        assert_eq!(reduce_optional_effects(Some(DENY), None), Some(DENY));
+        assert_eq!(reduce_optional_effects(Some(ALLOW), None), Some(ALLOW));
+        assert_eq!(reduce_optional_effects(None, Some(DENY)), Some(DENY));
+        assert_eq!(reduce_optional_effects(None, Some(ALLOW)), Some(ALLOW));
+
+        assert_eq!(reduce_optional_effects(None, None), None);
     }
 }
