f6af91defe93c4d5709c37f60aa796fbd44fd8de
Greg Wiley <aztec.rex@jammm.com>
2021-09-27 15:29:27 -0700

    reliable environment

--- src/environment.rs
+++ src/environment.rs
@@ -1,4 +1,6 @@
-//! Side-effectful computation context.
+//! Context for authorization resolution.
+//!
+//!
 
 /// Contextual computations. An environment is considered unreliable generally
 /// so its methods return a `Result` for error signaling.
@@ -14,33 +16,52 @@ pub trait Environment {
     fn test_condition(&self, exp: &Self::CExp) -> Result<bool, Self::Err>;
 }
 
+/// Contextual computations for environments that cannot fail when testing conditions.
+pub trait ReliableEnvironment {
+    /// The type of conditional expression that can be evaluated in the environment.
+    type CExp;
+
+    /// Test that a condition holds with respect to the environment. Can return
+    /// `Err(_)` if an environmental error is encountered.
+    fn reliably_test_condition(&self, exp: &Self::CExp) -> bool;
+}
+
+impl<T> Environment for T
+where
+    T: ReliableEnvironment,
+{
+    type CExp = <Self as ReliableEnvironment>::CExp;
+    type Err = ();
+
+    fn test_condition(&self, exp: &Self::CExp) -> Result<bool, Self::Err> {
+        Ok(self.reliably_test_condition(exp))
+    }
+}
+
 /// Environment in which conditions always match and evaluations never fail.
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub struct PositiveEnvironment;
-#[derive(PartialEq, Eq, Debug, Clone, Copy)]
-pub struct PositiveEnvironmentError;
+// #[derive(PartialEq, Eq, Debug, Clone, Copy)]
+// pub struct PositiveEnvironmentError;
 
-impl Environment for PositiveEnvironment {
-    type Err = PositiveEnvironmentError;
+impl ReliableEnvironment for PositiveEnvironment {
     type CExp = ();
 
-    fn test_condition(&self, _: &Self::CExp) -> Result<bool, Self::Err> {
-        Ok(true)
+    fn reliably_test_condition(&self, _: &Self::CExp) -> bool {
+        true
     }
 }
 
 /// Environment in which conditions never match and evaluations never fail.
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub struct NegativeEnvironment;
-#[derive(PartialEq, Eq, Debug, Clone, Copy)]
-pub struct NegativeEnvironmentError;
+// #[derive(PartialEq, Eq, Debug, Clone, Copy)]
+// pub struct NegativeEnvironmentError;
 
-impl Environment for NegativeEnvironment {
-    type Err = NegativeEnvironmentError;
+impl ReliableEnvironment for NegativeEnvironment {
     type CExp = ();
-
-    fn test_condition(&self, _: &Self::CExp) -> Result<bool, Self::Err> {
-        Ok(false)
+    fn reliably_test_condition(&self, _: &Self::CExp) -> bool {
+        false
     }
 }
 
@@ -48,6 +69,17 @@ impl Environment for NegativeEnvironment {
 mod tests {
     use super::*;
 
+    #[test]
+    pub fn test_positive_environment_reliably_matches() {
+        let env = &PositiveEnvironment;
+        assert_eq!(env.reliably_test_condition(&()), true);
+    }
+
+    #[test]
+    pub fn test_negate_environment_reliably_does_not_match() {
+        let env = &NegativeEnvironment;
+        assert_eq!(env.reliably_test_condition(&()), false);
+    }
     #[test]
     pub fn test_positive_environment_matches() {
         let env = &PositiveEnvironment;
