2a88439e4a2a9b769582c03e279342e8a0317f26
Greg Wiley <aztec.rex@jammm.com>
2022-08-18 07:59:43 -0700

    checkpoint

--- crates/core/src/enforcement.rs
+++ crates/core/src/enforcement.rs
@@ -1,50 +1,51 @@
-pub struct Authorization<Subj, Prin>(Prin, Subj, bool);
+pub struct Authorization<Subj>(Subj, bool);
 
-impl<Subj, Prin> Authorization<Subj, Prin> {
+impl<Subj> Authorization<Subj> {
     pub fn authorized(&self) -> bool {
-        self.2
+        self.1
     }
 
     pub fn subject(&self) -> &Subj {
-        &self.1
+        &self.0
+    }
+}
+
+pub struct Authorizations<Azn, Prin>(Prin, Vec<Azn>);
+
+impl<Subj, Prin> Authorizations<Authorization<Subj>, Prin> {
+    fn new<T: IntoIterator<Item = Authorization<Subj>>>(principal: Prin, items: T) -> Self {
+        Authorizations(principal, items.into_iter().collect())
+    }
+    pub fn authorized(&self) -> bool {
+        !self.1.is_empty() && self.1.iter().all(Authorization::authorized)
     }
 
     pub fn principal(&self) -> &Prin {
         &self.0
     }
-}
 
-pub struct Authorizations<Azn>(Vec<Azn>);
-
-impl<Subj, Prin> Authorizations<Authorization<Subj, Prin>> {
-    pub fn authorized(&self) -> bool {
-        !self.0.is_empty() && self.0.iter().all(Authorization::authorized)
+    pub fn as_slice(&self) -> &[Authorization<Subj>] {
+        self.1.as_slice()
     }
 
-    pub fn as_slice(&self) -> &[Authorization<Subj, Prin>] {
-        self.0.as_slice()
-    }
-
-    pub fn as_slice_mut(&mut self) -> &mut [Authorization<Subj, Prin>] {
-        self.0.as_mut_slice()
+    pub fn as_slice_mut(&mut self) -> &mut [Authorization<Subj>] {
+        self.1.as_mut_slice()
     }
 }
 
-impl<Subj, Prin> FromIterator<Authorization<Subj, Prin>>
-    for Authorizations<Authorization<Subj, Prin>>
-{
-    fn from_iter<T: IntoIterator<Item = Authorization<Subj, Prin>>>(items: T) -> Self {
-        Authorizations(items.into_iter().collect())
-    }
-}
+// impl<Subj, Prin> FromIterator<Authorization<Subj>> for Authorizations<Authorization<Subj>, Prin> {
+//     fn from_iter<T: IntoIterator<Item = Authorization<Subj>>>(items: T) -> Self {
+//         Authorizations(items.into_iter().collect())
+//     }
+// }
 
-impl<Subj, Prin> IntoIterator for Authorizations<Authorization<Subj, Prin>> {
-    type Item = Authorization<Subj, Prin>;
+impl<Subj, Prin> IntoIterator for Authorizations<Authorization<Subj>, Prin> {
+    type Item = Authorization<Subj>;
 
     type IntoIter = <Vec<Self::Item> as IntoIterator>::IntoIter;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
+        self.1.into_iter()
     }
 }
 
@@ -57,5 +58,5 @@ pub trait AuthorizationOracle {
         &self,
         principal: &Self::Principal,
         subjects: &[Self::Subject],
-    ) -> Result<Authorizations<Authorization<Self::Subject, Self::Principal>>, Self::Err>;
+    ) -> Result<Authorizations<Authorization<Self::Subject>, Self::Principal>, Self::Err>;
 }
